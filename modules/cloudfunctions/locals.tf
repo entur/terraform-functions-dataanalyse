c62ee79af5cd1875878c998c6b206374d7265aaf
marius-maudal <marius.maudal@entur.org>
2025-09-26 13:51:37 +0200

    Avoid logging_metric name collision when using multiple cloud functions; Sanitize name (#3)

--- modules/cloudfunctions/locals.tf
+++ modules/cloudfunctions/locals.tf
@@ -5,6 +5,10 @@ locals {
   # Service account email
   service_account_email = module.init.service_accounts.default.email
 
+  # Sanitized function name for resources that require restricted characters
+  function_name_compact = join("", regexall("[a-z0-9_]", lower(var.function_name)))
+  function_name_safe    = can(regex(local.function_name_compact, "^[a-z]")) ? local.function_name_compact : "f${local.function_name_compact}"
+
   # Location for all resources (function, scheduler, storage)
   location = var.location
 }
